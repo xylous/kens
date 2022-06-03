b98b93e9b02347daa40a462b287b27a875ea896f
xylous <xylous.e@gmail.com>
2022-06-03 20:12:27 +0300

    Create a 2D grid and print it

--- src/main.c
+++ src/main.c
@@ -41,7 +41,35 @@ struct SnakeNode *new_snakenode(int x, int y)
     return node;
 }
 
+/**
+ * Create a 2D array in heap of size [SCREEN_HEIGHT][SCREEN_WIDTH]
+ */
+int **create_grid(void)
+{
+    int **rows = calloc(SCREEN_HEIGHT, sizeof(int*));
+    int *values = calloc(SCREEN_WIDTH * SCREEN_HEIGHT, sizeof(int));
+
+    for (int i = 0; i < SCREEN_HEIGHT; i++) {
+        rows[i] = values + i * SCREEN_WIDTH;
+    }
+
+    return rows;
+}
+
 int main(int argc, char **argv)
 {
     printf("Hello snake!\n");
+
+    int **grid = create_grid();
+
+    for (int i = 0; i < SCREEN_HEIGHT; i++) {
+        for (int j = 0; j < SCREEN_WIDTH; j++) {
+            if (grid[i][j] != 0) {
+                printf("%d", grid[i][j]);
+            } else {
+                printf("-");
+            }
+        }
+        printf("\n");
+    }
 }
