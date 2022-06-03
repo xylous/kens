a47e5f82ec9abef3ed7ba73f6ab0947cb5553d87
xylous <xylous.e@gmail.com>
2022-06-03 19:48:40 +0300

    Define data types

--- src/main.c
+++ src/main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define SCREEN_WIDTH 10
+#define SCREEN_HEIGHT 10
+
+/**
+ * Linked lists.
+ */
+struct SnakeNode
+{
+    int x;
+    int y;
+    struct SnakeNode *next;
+};
+
 int main(int argc, char **argv)
 {
     printf("Hello snake!\n");
