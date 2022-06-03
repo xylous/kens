a568e6e8649f726e9b4aef06558d949413b191db
xylous <xylous.e@gmail.com>
2022-06-03 19:54:40 +0300

    Add GPLv3 license boilerplate

--- src/main.c
+++ src/main.c
@@ -1,3 +1,19 @@
+/* Copyright (c) 2022 xylous <xylous.e@gmail.com>
+ *
+ * This file is part of kens.
+ * kens is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * kens is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * kens.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
