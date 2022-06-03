5f4b251af515efd28580baa3b26b3048ed2d7ca5
xylous <xylous.e@gmail.com>
2022-06-03 19:53:40 +0300

    Add constructor for SnakeNode

--- src/main.c
+++ src/main.c
@@ -14,6 +14,17 @@ struct SnakeNode
     struct SnakeNode *next;
 };
 
+/**
+ * Create a new SnakeNode at the specified position
+ */
+struct SnakeNode *new_snakenode(int x, int y)
+{
+    struct SnakeNode *node = calloc(1, sizeof(struct SnakeNode));
+    node->x = x;
+    node->y = y;
+    return node;
+}
+
 int main(int argc, char **argv)
 {
     printf("Hello snake!\n");
